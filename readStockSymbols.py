ce46b88606c0f51cef8f0e62d7dbeef3be745975
Tsung Chieh Chen <d12300d@gmail.com>
2023-02-20 13:09:59 -0800

    split stock symbol into smaller csv by market cap

--- readStockSymbols.py
+++ readStockSymbols.py
@@ -6,5 +6,30 @@ df = pd.read_csv('./dataset/Stocks Symbols.csv')
 df = df.drop(["Last Sale", "Net Change", "% Change",
              "IPO Year"], axis='columns')
 
-print(df.head(10))
-print(len(df))
+# sort df by "Market Cap"
+df = df.sort_values("Market_Cap", ascending=False)
+
+# print(df.head(10))
+# print(len(df))
+
+# split whole NASDAQ into mega, large, and mid cap dataframe
+
+# mega
+mega_cap_df = df.query('Market_Cap > 200*(10**9)')
+df.to_csv("./dataset/MegaCap Stock Symbols", index=False)
+print(mega_cap_df.head())
+print(len(mega_cap_df))
+
+
+# large
+large_cap_df = df.query('10 * (10**9) < Market_Cap < 200*(10**9)')
+df.to_csv("./dataset/LargeCap Stock Symbols", index=False)
+print(large_cap_df.head())
+print(len(large_cap_df))
+
+
+# mid
+mid_cap_df = df.query('2 * (10**9) < Market_Cap < 200*(10**9)')
+df.to_csv("./dataset/MidCap Stock Symbols", index=False)
+print(mid_cap_df.head())
+print(len(mid_cap_df))
