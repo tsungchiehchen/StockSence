e94f4a8d5864890b6169f2a4efa229d8f6ec1d74
Chen-Yu Yu <tobyyu007@gmail.com>
2023-03-08 02:09:08 -0800

    將 treemap 和 calculateStockChangebyDate 接起來、和 Vue 接起來、調整漲跌顏色顯示

---
+++ backend/dataModule/controller.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import json
+from collections import defaultdict
+import sys
+
+
+def process(titlesPath, journalsPath):
+    print("controller")
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, session, redirect
+from dataModule import calculateStockChangebyDate
 
-app = Flask(__name__, static_url_path='', static_folder='static')
+app = Flask(__name__, static_url_path='', static_folder='../frontend/flask/static', template_folder='../frontend/flask/template')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -8,7 +9,7 @@ def index():
     print(request.method)
     if request.method == 'POST':
         if 'type' in request.form:
-            print(request.form)
+            calculateStockChangebyDate.processAllStocksChange(request.form['startDate'], request.form['endDate'])
             return redirect(request.path,code=302)
         # if request.form:
         #     type = request.form['type']
