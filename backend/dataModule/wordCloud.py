85122d5fa4d56811bd5d1195cbbf8e74c4a80c07
Tsung-Chieh Chen <d12300d@gmail.com>
2023-03-14 20:17:59 -0700

    update wordcloud's json output format

--- backend/dataModule/wordCloud.py
+++ backend/dataModule/wordCloud.py
@@ -1,7 +1,28 @@
 import pandas as pd
 import operator
 import json
-from nlp import preprocess
+import re
+from nltk.corpus import stopwords
+import itertools
+
+
+def preprocess(raw_text: str):
+    stop_words = set(stopwords.words('english'))
+
+    # Step 1: remove non-letter characters and convert the string to lower case
+    letters_only_text: str = re.sub("[^a-zA-Z]", " ", raw_text)
+    letters_only_text: str = letters_only_text.lower()
+
+    # Step 2: tokenization -- split into words -> convert string into list ( 'hello world' -> ['hello', 'world'])
+    words: list[str] = letters_only_text.split()
+
+    # Step 3: remove stopwords
+    cleaned_words = []
+    for word in words:
+        if word not in stop_words:
+            cleaned_words.append(word)
+
+    return " ".join(cleaned_words)
 
 
 def getDFbyDate(start_date, end_date, symbol):
@@ -38,20 +59,26 @@ def getWordCloud(start_date, end_date, symbol):
 
         if polarity > 0:
             for t in title:
-                if t in frequency:
-                    frequency[t] += 1
-                else:
-                    frequency[t] = 1
+                if len(t) > 1:
+                    t += '+'
+                    if t in frequency:
+                        frequency[t] += 1
+                    else:
+                        frequency[t] = 1
         elif polarity < 0:
             for t in title:
-                if t in frequency:
-                    frequency[t] -= 1
-                else:
-                    frequency[t] = -1
+                if len(t) > 1:
+                    t += '-'
+                    if t in frequency:
+                        frequency[t] += 1
+                    else:
+                        frequency[t] = 1
 
     # Sort the frequency dict by freq in descending order
     sorted_freq = dict(
         sorted(frequency.items(), key=operator.itemgetter(1), reverse=True))
+    # get the first 150 items
+    sorted_freq = dict(itertools.islice(sorted_freq.items(), 150))
 
     # convert to list of dictionaries
     data = [{'name': k, 'value': v} for k, v in sorted_freq.items()]
@@ -61,7 +88,7 @@ def getWordCloud(start_date, end_date, symbol):
 
 
 # testing
-# start_date = "2020-01-01"
-# end_date = "2022-01-01"
-# symbol = 'AAPL'
-# getWordCloud(start_date, end_date, symbol)
+start_date = "2020-01-01"
+end_date = "2022-01-01"
+symbol = 'AAPL'
+getWordCloud(start_date, end_date, symbol)
